0ea2ab41840d84bd949f1815b6c9d16fba4b1ba0
Nathanial <goldengollem@gmail.com>
2017-02-09 09:00:08 -0600

    Merged To Command Base

---
+++ src/org/usfirst/frc/team2509/robot/commands/GyroTurn.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc2509.Robot.commands;
+
+import org.usfirst.frc2509.Robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class GyroTurn extends Command {
+	private Command OpDrive = new OpDrive();
+	private Joystick stick = Robot.oi.getOpStick();
+    public GyroTurn() {
+    	
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(stick.getPOV()==0){
+    		
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
